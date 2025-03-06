ab2d8afaec090f5b331fc3026db6513ddc718a3e
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-06 13:23:00 -0700

    Renamed loginPage.css file

--- Frontend/dupe-machine/src/pages/LoginPage.js
+++ Frontend/dupe-machine/src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../styling/TempLoginPage.css";
+import "../styling/LoginPage.css";
 
 function LoginPage() {
 
