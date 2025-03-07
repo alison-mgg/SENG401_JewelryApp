8c2f1c9ef8542d92aead24806e32677535bfdc56
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-07 14:17:28 -0700

    Creating nav bar

--- Frontend/dupe-machine/src/pages/MainPage.js
+++ Frontend/dupe-machine/src/pages/MainPage.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/MainPage.css";
+import NavBar from "../components/NavBar/NavBar.js";
 
 function MainPage() {
-
   return (
     <div className="mainpage-container">
-    <h1 className="page-title">Test: Welcome to The Main Page</h1>
+      <NavBar />
+      <h1 className="page-title">Test: Welcome to The Main Page</h1>
     </div>
-);
+  );
 }
 
 export default MainPage;
