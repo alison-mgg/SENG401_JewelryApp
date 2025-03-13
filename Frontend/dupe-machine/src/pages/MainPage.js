7c0b8aa512933cf0b58454410df6589a77a8faf1
alison.mgg <alison.gartner@ucalgary.ca>
2025-03-12 19:08:56 -0600

    nav bar complete

--- Frontend/dupe-machine/src/pages/MainPage.js
+++ Frontend/dupe-machine/src/pages/MainPage.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/MainPage.css";
+import NavigationBar from "./NavigationBar.js";
 
 function MainPage() {
-
   return (
     <div className="mainpage-container">
-    <h1 className="page-title">Test: Welcome to The Main Page</h1>
+      <NavigationBar />
+      <h1 className="page-title">Test: Welcome to The Main Page</h1>
     </div>
-);
+  );
 }
 
 export default MainPage;
