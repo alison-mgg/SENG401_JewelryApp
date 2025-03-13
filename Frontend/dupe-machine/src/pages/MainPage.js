6cabd9f1cd86105e0803056cd740423cbed54e23
alison.mgg <alison.gartner@ucalgary.ca>
2025-03-12 19:12:03 -0600

    fixed main page

--- Frontend/dupe-machine/src/pages/MainPage.js
+++ Frontend/dupe-machine/src/pages/MainPage.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/MainPage.css";
 
-import NavBar from "../components/NavBar/NavBar.js";
- main
+import NavBar from "./NavigationBar.js";
 
 function MainPage() {
   return (
     <div className="mainpage-container">
 
       <NavBar />
- main
       <h1 className="page-title">Test: Welcome to The Main Page</h1>
     </div>
   );
