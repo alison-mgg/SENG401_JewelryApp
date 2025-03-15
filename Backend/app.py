488700f12dc3e766b86936d2687e1a1a87629a1a
Zaira Ramji <zairaram@gmail.com>
2025-03-15 00:18:04 -0600

    update to use db connector file

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from database_connector import get_database
 #from gemini_config.settings import UPLOAD_FOLDER
 #from routes.image_routes import image_routes
 #from routes.description_routes import description_routes
-#^^These imports willl be integrated once the ai connection branch is ready
+#^^These imports will be integrated once the ai connection branch is ready
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
+
+# MySQL Database configuration using environment variables
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
 
 #For ai integration branch:
 #app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
