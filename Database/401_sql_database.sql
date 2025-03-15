b212d7da8cdcba2e4d18fd963f2ce0009d9b0370
Zaira Ramji <zairaram@gmail.com>
2025-03-15 00:17:11 -0600

    add users table

--- Database/401_sql_database.sql
+++ Database/401_sql_database.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXISTS `JewelryApp`;
 CREATE DATABASE `JewelryApp`;
 USE JewelryApp;
+
+DROP USER IF EXISTS 'seng401'@'localhost';
+CREATE USER 'seng401'@'localhost' IDENTIFIED BY 'seng401';
+GRANT ALL PRIVILEGES ON `JewlryApp`.* TO 'seng401'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,    
+    username VARCHAR(255) NOT NULL,       
+    email VARCHAR(255) NOT NULL UNIQUE,    
+    password VARCHAR(255) NOT NULL,        -- Ideally, password hashed on server side
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
