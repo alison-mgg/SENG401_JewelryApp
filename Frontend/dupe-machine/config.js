e0f8a9699cc0e7cf1e53efa1953a73335efa66c1
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-16 14:45:37 -0600

    Updated comment block for config.js file

--- Frontend/dupe-machine/config.js
+++ Frontend/dupe-machine/config.js
@@ -1,3 +1,9 @@
+/* Config.js File
+ * This file contains the configuration for the frontend application, such as the
+ * API URL and the environment (development or production). Helps to manage
+ * the connections to backend hosted on AWS Elastic Beanstalk, ensuring consistent
+ * setup across all components of the application for API calls.
+ */
 const config = {
     apiURL: 'http://jewelry-app-env.eba-r9q3svxm.ca-central-1.elasticbeanstalk.com',
     environment: 'development'  // Change depending on whether development or production
