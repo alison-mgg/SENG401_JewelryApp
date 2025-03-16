7bb920f0fc9dff65e89ebd1a6e9e37d433b35aa0
Maryam <maryamsalsayed@gmail.com>
2025-03-15 22:20:26 -0600

    added chats table to sql file

--- Database/401_sql_database.sql
+++ Database/401_sql_database.sql
@@ -18,3 +18,11 @@ CREATE TABLE users (
 -- Test user:
 INSERT INTO users (username, email, password) 
 VALUES ('a', 'a@a.com', 'a');
+
+CREATE TABLE chats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) REFERENCES users(username),
+    img_path TEXT NOT NULL,
+    links TEXT NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
