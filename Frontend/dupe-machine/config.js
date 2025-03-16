a8e0f1583076532d19cbb8789fd29c3941cf35cc
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-16 14:42:02 -0600

    Create a config.js file to reference the AWS Backend API for the frontend

---
+++ Frontend/dupe-machine/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiURL: 'http://jewelry-app-env.eba-r9q3svxm.ca-central-1.elasticbeanstalk.com',
+    environment: 'development'  // Change depending on whether development or production
+};
+
+export default config;
