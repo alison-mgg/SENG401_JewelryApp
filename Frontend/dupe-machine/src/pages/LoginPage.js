d80d6cda25e01958179780cf904df738b348d861
Zaira Ramji <zairaram@gmail.com>
2025-03-15 21:36:22 -0600

    started backend integration

--- Frontend/dupe-machine/src/pages/LoginPage.js
+++ Frontend/dupe-machine/src/pages/LoginPage.js
@@ -1,17 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+//import { useAuthContext } from '../App.js';
 import "../styling/LoginPage.css";
 
 function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState({ type: '', content: '' }); // State to manage the message
+  //const { setAuthenticated } = useAuthContext(); // Access setAuthenticated function from context
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    if (username.trim() === '' || password.trim() === '') {
+        displayMessage('error', 'Username and password are required.');
+        return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:5000/api/', { 
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        setMessage({ type: 'success', content: 'Authentication successful' });
+        console.log('success')
+        //setAuthenticated(true);
+        navigate('/main'); // Redirect to main page after successful authentication
+      } else {
+          setMessage({ type: 'error', content: 'Authentication failed. Incorrect username or password.' });
+      }
+  } catch (error) {
+    console.error('Error occurred during authentication:', error);
+    setMessage({ type: 'error', content: 'Authentication failed. Please try again.' });
+  }
+
+  };
+
+  const displayMessage = (type, content) => {
+    setMessage({ type, content });
+    setTimeout(() => setMessage({ type: '', content: '' }), 50000); // Clear the message after 50 seconds
+  };  
+
+
   return (
     <div className="login-container">
       <h1 className="page-title">Welcome to The Dupe Machine</h1>
       <h2 className="login-title">Login</h2>
-      <div className="login-box">
-        <input type="text" placeholder="Username" className="input-field" />
-        <input type="password" placeholder="Password" className="input-field" />
-        <button className="login-button">Login</button>
-      </div>
+      {/* Display message if it exists */}
+      {message.content && (
+                <div className={`message-box ${message.type}`}>
+                    <p>{message.content}</p>
+                </div>
+            )}
+      <form className="login-box">
+        <input type="text" placeholder="Username" className="input-field" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" className="input-field" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="login-button" onClick={handleSubmit}>Login</button>
+      </form>
       <h3 className="no-account-title">Don't have an account?</h3>
       <div className="options">
         <Link to="/signup">
