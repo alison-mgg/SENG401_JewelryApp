1c9cd93f035bc1f8724ae24e8938a76a38582e51
Zaira Ramji <zairaram@gmail.com>
2025-03-15 22:17:18 -0600

    changed login url

--- Frontend/dupe-machine/src/pages/LoginPage.js
+++ Frontend/dupe-machine/src/pages/LoginPage.js
@@ -19,7 +19,7 @@ function LoginPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/', { 
+      const response = await fetch('http://localhost:5000/api/login', { 
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
