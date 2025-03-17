cb32ff7849f8c651faef782557ded7f39de5cc65
alison.mgg <alison.gartner@ucalgary.ca>
2025-03-17 04:52:28 -0600

    profile page added to branch

--- Frontend/dupe-machine/src/pages/ProfilePage.js
+++ Frontend/dupe-machine/src/pages/ProfilePage.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "../styling/ProfilePage.css";
 import NavBar from "./NavigationBar.js";
+import UserDetails from "./UserDisplay.js";
+import Chats from "./SavedChats.js";
 
 function ProfilePage() {
 
   return (
     <div className="profilepage-container">
      <NavBar />
-    <h1 className="page-title">Test: ProfilePage</h1>
+    <UserDetails/>
+    <Chats />
     </div>
 );
 }
+
 
 export default ProfilePage;
