fbc57a97310da84677bcebf2ae98a2fa44427d30
Zaira Ramji <zairaram@gmail.com>
2025-03-18 14:31:54 -0600

    full app runs from app.py in "Gemini_Testing"

--- Backend/database_connector.py
+++ Backend/database_connector.py
@@ -1,15 +1,16 @@
 #Connect python to mySQL, to import in controller files
 import mysql.connector
 from flask import current_app
+import os
 
 def get_database():
     """Get a MySQL database connection using mysql.connector."""
     if not hasattr(current_app, 'database'):
         # Initialize the connection if it's not already set
         current_app.database = mysql.connector.connect(
-            host=current_app.config['MYSQL_HOST'],
-            user=current_app.config['MYSQL_USER'],
-            password=current_app.config['MYSQL_PASSWORD'],
-            database=current_app.config['MYSQL_DB']
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DB")
         )
     return current_app.database
