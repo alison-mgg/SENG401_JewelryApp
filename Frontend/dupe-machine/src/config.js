886f87ab4f34baf8ca0c122e1fee2bf7c08f400b
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-18 22:25:32 -0600

    Update API URL in config to point to Render deployment

--- Frontend/dupe-machine/src/config.js
+++ Frontend/dupe-machine/src/config.js
@@ -1,13 +1,12 @@
 /* Config.js File
  *
  * This file contains the configuration for the frontend application, such as the
- * API URL and the environment (development or production). Helps to manage
- * the connections to backend hosted on AWS Elastic Beanstalk, ensuring consistent
+ * API URL. Helps to manage the connections to backend hosted on Render, ensuring consistent
  * setup across all components of the application for API calls.
  */
 const config = {
-    apiURL: 'http://jewelry-app-env.eba-r9q3svxm.ca-central-1.elasticbeanstalk.com',
-    environment: 'development'  // Change depending on whether development or production
+    apiURL: 'https://jewelry-dupe-app.onrender.com', // Updated to Render link
+    // apiURL: 'http://localhost:5000', // Original link
 };
-
-export default config;
+  
+  export default config;
