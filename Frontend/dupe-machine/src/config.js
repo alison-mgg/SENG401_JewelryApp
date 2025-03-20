92d1a0c87050d3235f822b35e8bb1a432c560749
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-19 20:28:53 -0600

    refactor: update API and CORS proxy configuration for environment variables

--- Frontend/dupe-machine/src/config.js
+++ Frontend/dupe-machine/src/config.js
@@ -4,16 +4,18 @@
  * API URL. Helps to manage the connections to backend hosted on Render, ensuring consistent
  * setup across all components of the application for API calls.
  */
-// const config = {
-//     apiURL: 'https://jewelry-dupe-app.onrender.com', // Updated to Render link
-//     // apiURL: 'http://localhost:5000', // Original link
-// };
-  
-//   export default config;
+const environment = process.env.NODE_ENV; // 'development' or 'production'
 
 const config = {
-    apiURL: 'https://jewelry-dupe-app.onrender.com',
-    corsProxyURL: 'https://cors-anywhere-6vpc.onrender.com/'
+  apiURL: environment === 'production'
+    ? process.env.REACT_APP_BACKEND_URL_PRODUCTION
+    : process.env.REACT_APP_BACKEND_URL_LOCAL,
+  corsProxyURL: process.env.REACT_APP_CORS_PROXY_URL,
 };
+
+// Check if a CORS proxy is being used
+if (config.corsProxyURL) {
+  config.apiURL = `${config.corsProxyURL}/${config.apiURL}`;
+}
 
 export default config;
