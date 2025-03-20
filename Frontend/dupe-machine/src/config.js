d26bcef4c6110cc2d357043cd2971f31e75607a4
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-20 16:21:38 -0600

    fix: update environment variables for Backend and Gemini Testing applications

--- Frontend/dupe-machine/src/config.js
+++ Frontend/dupe-machine/src/config.js
@@ -4,21 +4,7 @@
  * API URL. Helps to manage the connections to backend hosted on Render, ensuring consistent
  * setup across all components of the application for API calls.
  */
-// const environment = process.env.NODE_ENV; // 'development' or 'production'
 
-// const config = {
-//   apiURL: environment === 'production'
-//     ? process.env.REACT_APP_BACKEND_URL_PRODUCTION
-//     : process.env.REACT_APP_BACKEND_URL_LOCAL,
-//   corsProxyURL: process.env.REACT_APP_CORS_PROXY_URL,
-// };
-
-// // Check if a CORS proxy is being used
-// if (config.corsProxyURL) {
-//   config.apiURL = `${config.corsProxyURL}/${config.apiURL}`;
-// }
-
-// export default config;
 
 const useLocalApi = false; // Set to true for local, false for remote
 
