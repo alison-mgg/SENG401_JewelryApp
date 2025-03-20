3af56797d6f91386ee811818355f3df33bde53e3
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-20 02:14:35 -0600

    chore: update .gitignore to exclude node_modules and Python bytecode files; clean up LoginPage.js by removing unused code

--- Frontend/dupe-machine/src/pages/LoginPage.js
+++ Frontend/dupe-machine/src/pages/LoginPage.js
@@ -1,55 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 //import { useAuthContext } from '../App.js';
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-//import { useAuthContext } from '../App.js';
 import "../styling/LoginPage.css";
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState({ type: '', content: '' }); // State to manage the message
-  //const { setAuthenticated } = useAuthContext(); // Access setAuthenticated function from context
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-
-    if (username.trim() === '' || password.trim() === '') {
-        displayMessage('error', 'Username and password are required.');
-        return;
-    }
-
-    try {
-      const response = await fetch('http://localhost:5000/api/login', { 
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, password }),
-      });
-      if (response.ok) {
-        setMessage({ type: 'success', content: 'Authentication successful' });
-        console.log('success')
-        //setAuthenticated(true);
-        navigate('/main'); // Redirect to main page after successful authentication
-      } else {
-          setMessage({ type: 'error', content: 'Authentication failed. Incorrect username or password.' });
-      }
-  } catch (error) {
-    console.error('Error occurred during authentication:', error);
-    setMessage({ type: 'error', content: 'Authentication failed. Please try again.' });
-  }
-
-  };
-
-  const displayMessage = (type, content) => {
-    setMessage({ type, content });
-    setTimeout(() => setMessage({ type: '', content: '' }), 50000); // Clear the message after 50 seconds
-  };  
-
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState({ type: '', content: '' }); // State to manage the message
