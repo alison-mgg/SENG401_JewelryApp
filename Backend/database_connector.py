9af15a2c36f0262115cc8f2a230fdb26ea2de828
Maryam <maryamsalsayed@gmail.com>
2025-03-21 09:52:17 -0600

    database connection fixed

--- Backend/database_connector.py
+++ Backend/database_connector.py
@@ -1,16 +1,16 @@
 #Connect python to mySQL, to import in controller files
 import mysql.connector
-from flask import current_app
+from flask import g
 import os
 
 def get_database():
     """Get a MySQL database connection using mysql.connector."""
-    if not hasattr(current_app, 'database'):
+    if 'database' not in g:
         # Initialize the connection if it's not already set
-        current_app.database = mysql.connector.connect(
+        g.database = mysql.connector.connect(
             host=os.getenv("MYSQL_HOST"),
             user=os.getenv("MYSQL_USER"),
             password=os.getenv("MYSQL_PASSWORD"),
             database=os.getenv("MYSQL_DB")
         )
-    return current_app.database
+    return g.database
